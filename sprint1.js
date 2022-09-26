7b51211519c3dacfba23686565c9acf51136b80c
Lucas Tadeu <ldelavor@hotmail.com>
2022-09-25 23:10:26 -0300

    Avançando em responsividade e javascript do desafio

---
+++ sprint1.js
@@ -0,0 +1,34 @@
+var mensagemDigitada = document.getElementById("message");
+var criptografar = document.getElementById("criptografar");
+var descriptografar = document.getElementById("descriptografar");
+
+
+/*
+function criptografia() {
+
+}
+
+function descriptografia() {
+
+}
+*/
+criptografar.onclick = function() {
+    console.log(mensagemDigitada.value);
+    
+}
+/*
+descriptografar.onclick = descriptografia;
+
+
+/*  if (letra == a){
+        letra = ai;
+} else if (letra == e){
+        letra = enter;
+} else if (letra == i){
+        letra = imes;
+} else if (letra == o){
+        letra = ober;
+} else if (letra == u){
+        letra = ufat;
+}
+   */
